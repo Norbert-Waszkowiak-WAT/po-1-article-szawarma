41122cbc7b47d0f0c8617a120b96d048d2c3213f
szawarma <dawidoszczak035@gmail.com>
2024-11-25 09:36:34 +0000

    ohio

--- book.cpp
+++ book.cpp
@@ -1,47 +1,56 @@
 #ifndef BOOK
 #define BOOK
+
 #include <string>
 #include <iostream>
-#include "chapter.cpp"
+#include <vector>
 #include "author.cpp"
-#include "article.cpp"
+#include "chapter.cpp"
+
 using namespace std;
 
-class Book
-{
-    private:
-        string title;
-        Author author;
-        int publicationYear;
-        vector <Chapter> chapters;
+class Book {
+private:
+    string title;
+    Author author;
+    int publicationYear;
+    vector<Chapter> chapters;
 
-    public:
-        Book(): title(""), author(Author()), publicationYear(0), chapters() {};
-        
-        Book(string bookTitle, Author bookAuthor, int year, vector <Chapter> &bookChapters)
-        : title(bookTitle), author(bookAuthor), publicationYear(year), chapters(bookChapters) {};
-        
-        void addChapter(const Chapter &chapter){
+public:
+    Book() {}
+
+    Book(string bookTitle, Author bookAuthor, int year, vector<Chapter>& bookChapters)
+        : title(bookTitle), author(bookAuthor), publicationYear(year), chapters(bookChapters) {}
+
+    void addChapter(Chapter& chapter) {
         chapters.push_back(chapter);
-        }
-        void displayInfo(){
-            cout << title << " ";
-            cout << author.getName() << " ";
-            cout << author.getSurname() << " ";
-            cout << publicationYear << " ";
-            cout << chapters.size() << endl;
-        };
-        string getTitle(){
-            return title;
-        };
-        Author getAuthor(){
-            return author;
-        };
-        int getPublicationYear(){
-            return publicationYear;
-        };
-        vector<Chapter> getChapters(){
-            return chapters;
-        };
+    }
+
+    void displayInfo() {
+        cout << "Title: " << title << endl;
+        cout << "Author: " << author.getName() << " " << author.getSurname() << endl;
+        cout << "Publication Year: " << publicationYear << endl;
+        cout << "Chapters: " << endl;
+
+    string getTitle() 
+    {
+        return title;
+    }
+
+    Author getAuthor() 
+    {
+        return author;
+    }
+
+    int getPublicationYear() 
+    {
+        return publicationYear;
+    }
+
+    vector<Chapter> getChapters() 
+    {
+        return chapters;
+    }
 };
+
 #endif
